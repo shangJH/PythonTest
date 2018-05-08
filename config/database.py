b70c2e83d89570693f996b9b5303ca685cb54b0e
shencong1992 <2894577759@qq.com>
2018-05-08 20:59:34 +0800

    # update readme.md

--- config/database.py
+++ config/database.py
@@ -1,3 +1,26 @@
 '''
-   数据库相关配置，写至此处
+    相关配置，写至此处
 '''
+
+# 发送邮件相关设置信息
+from_addr='m18054293763@163.com'
+password='shencong1992'
+to_addr='2894577759@qq.com'    #发送地址
+smtp_server='smtp.163.com'     #stmp服务器地址
+
+# MongoDB
+mongo_url='127.0.0.1'
+
+# Redis
+redis_url='127.0.0.1'
+pass_key='shencong'
+
+# MySQL
+database_url='127.0.0.1'
+database_port=3306
+username='root'
+password='shencong'
+
+# 目前熟练使用3个主流数据库即可，后面在慢慢操作HBase和HDFS等
+
+
